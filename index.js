3de1f0bdc3847917668f42bcc6356fdb9b03c22e
Jason Leach <jlspursfan@gmail.com>
2019-05-04 02:11:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ function theBeatlesPlay(musicians, instruments) {
 
 function johnLennonFacts(facts) {
   var new_facts = []
-//  while (var i = 0; (i < facts.length); i++){
-//    new_facts[i] = facts[i] + "!!!"
-//  }
-  return new_facts
+  while (var i = 0; (i < facts.length); i++) {
+//    new_facts[i] = facts[i] + "!!!";
+  }
+  return new_facts;
 }
 
 function iLoveTheBeatles(num) {
